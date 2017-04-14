bdfb2653cbbe1cf000ea682129ca967c171d8a7a
oded <oded123456@gmail.com>
2017-04-14 13:38:23 +0300

    master: Moving type checks out of types.c

--- include/type_check.h
+++ include/type_check.h
@@ -3,6 +3,13 @@
 
 #include <types.h>
 
-/* TODO: move type check definitions here */
+bool type_check_expression(
+	type_space_t *type_space,
+	statement_expression_t *expression,
+	closure_t *closure,
+	/* OUT */ type_t ** expression_type
+);
+
+bool type_check_block(type_space_t *type_space, code_block_t *code_block, closure_t *closure);
 
 #endif
